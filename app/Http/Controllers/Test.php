a26e8f3506e83831492b424832fe5acbbc707072
clover <370546391@qq.com>
2016-03-28 15:10:29 +0800

    基础框架

---
+++ app/Http/Controllers/Test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/3/28
+ * Time: 14:35
+ */
+
+namespace App\Http\Controllers;
+
+class Test extends Controller
+{
+    public function t()
+    {
+
+    }
+}
