e51d0b8466348473648cfbf988939db0ad395b5c
clover <370546391@qq.com>
2016-04-14 11:48:51 +0800

    改用lumen

---
+++ app/Http/Controllers/Admin/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/4/14
+ * Time: 11:20
+ */
+
+namespace App\Http\Controllers\Admin;
+
+
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+
+    public function login()
+    {
+        return 'sadasdsa';
+    }
+
+}
---
+++ app/Http/routes.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$app->get('/', function () use ($app) {
+    return $app->version();
+});
+
+$app->get('auth/signIn' , 'Admin\AuthController@login');
+
+$app->group(['namespace' => 'App\Http\Controllers\Admin' , 'prefix' => 'admin'] , function() use ($app) {
+
+
+
+});
+
