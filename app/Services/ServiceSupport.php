1fe57a76525c04e0b83d71d17c60c5f1ec277dae
clover <370546391@qq.com>
2016-04-16 14:49:03 +0800

    添加 service 前后置方法调用

--- app/Services/ServiceSupport.php
+++ app/Services/ServiceSupport.php
@@ -9,6 +9,8 @@
 namespace App\Services;
 
 
+use Illuminate\Support\MessageBag;
+
 abstract class ServiceSupport
 {
 
@@ -16,11 +18,34 @@ abstract class ServiceSupport
 
     protected $providers = [];
 
+    //方法调用前置，在方法执行之前调用
+    protected $executePrefix = '__before';
+
+    //方法调用后置，在方法执行之后调用
+    protected $executeSuffix = '__after';
+
+    /**
+     * @var \Illuminate\Contracts\Support\MessageBag $error
+     */
+    protected $error;
+
+    public function __construct()
+    {
+        $this->error = app(MessageBag::class);
+    }
+
     public function __get($name)
     {
-        if(isset($this->providers[$name])) {
+        if (isset($this->providers[$name])) {
             return app()->make($this->providers[$name]);
         } else {
+
+            $method = 'get' . ucfirst($name);
+
+            if (method_exists($this, $method)) {
+                $this->attributes[$name] = call_user_func_array([$this, $method], []);
+            }
+
             return isset($this->attributes[$name]) ? $this->attributes[$name] : null;
         }
     }
@@ -30,5 +55,24 @@ abstract class ServiceSupport
         $this->attributes[$name] = $value;
     }
 
+    public function __call($method, $arguments)
+    {
+        if (method_exists($this, $method)) {
 
+            $beforeMethod = $method . $this->executePrefix;
+            $afterMethod = $method . $this->executeSuffix;
+
+            if (method_exists($this, $beforeMethod)) {
+                if (!app()->call([$this, $beforeMethod])) {
+                    throw new \Exception('method not found');
+                }
+            }
+
+            $result = call_user_func_array([$this, $method], $arguments);
+
+            return method_exists($this, $afterMethod) ? app()->call([$this , $afterMethod] , [$result]) : $result;
+        }
+
+        throw new \Exception('method not found');
+    }
 }
